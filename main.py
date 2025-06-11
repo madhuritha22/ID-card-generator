707d0987190c22dd2ae4c0c5fdae2c0f4cc8cf69
Madhuritha01 <alle_841969@student.nitw.ac.in>
2025-06-11 01:19:47 -0500

    Generating ID card for all employees

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import csv
+import os
 from PIL import Image, ImageDraw, ImageFont
+
+
+ids="final_templates"
+def generate_id(row):
+    template=Image.open('ute_id_template.png')
+    draw=ImageDraw.Draw(template)
+
+    font = ImageFont.truetype("georgia.ttf", size=25) 
+    text = row[0]
+    text_color = (255,255,255)
+    text_position = (75, 165)
+
+    draw.text(text_position, text, font=font, fill=text_color)
+
+    template = template.convert("RGB")
+
+    path = row[2]
+    employee_photo=Image.open(path)
+    employee_photo=employee_photo.resize((121,121))
+    template.paste(employee_photo, (176, 19))
+    filename = f"{row[0].replace(' ', '_')}.jpg"  # e.g., George_Cooper.jpg
+    save_path = os.path.join(ids, filename) 
+    template.save(save_path)
+    print(f"Saved: {save_path}")
+    template.show() 
+
+
 with open('employee_data.csv','r') as employee_data:
     csv_reader= csv.reader(employee_data)
-
-    for name,role,loc in csv_reader:
-     print(name,role,loc)
-
-template=Image.open('ute_id_template.png')
-#template.show()
-
-draw=ImageDraw.Draw(template)
-
-font = ImageFont.truetype("georgia.ttf", size=25) 
-text = "George Cooper"
-text_color = (255,255,255)
-text_position = (75, 165) # x, y coordinates for the top-left corner of the text
-
-draw.text(text_position, text, font=font, fill=text_color)
-
-template = template.convert("RGB")
-
-
-employee_photo=Image.open('employee_images\george.jpg')
-employee_photo=employee_photo.resize((121,121))
-template.paste(employee_photo, (176, 19)) 
-template.save("output_image.jpg") # Save the image
-template.show() # Display the image (optional)
-#employee_photo.show()
+    header=next(csv_reader)
+    for row in csv_reader:
+     print(row)
+     generate_id(row)   
